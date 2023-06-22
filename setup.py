bec135ccb08d3da9851b5181f602dd5375495f90
Florian Matter <florianmatter@gmail.com>
2023-06-21 17:30:10 -0700

    deps

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setup(
         "clld_markdown_plugin",
         "clld_morphology_plugin",
         "clld_corpus_plugin",
-        "tqdm"
+        "tqdm",
+        "waitress"
     ],
     extras_require={
         "dev": ["flake8", "waitress"],
