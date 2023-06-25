35ec8f0664398e3dd07b9e473e65cad2741999fd
Florian Matter <florianmatter@gmail.com>
2023-06-25 12:26:27 -0700

    different setup
    Former-commit-id: c749de30326dfb4cd27e78df4f83b0c578cfadf0

--- runapp.py
+++ runapp.py
@@ -2,8 +2,9 @@ import os
 
 from paste.deploy import loadapp
 from waitress import serve
-
+import shutil
 if __name__ == "__main__":
+    shutil.copy("development_tmpl.ini", "development.ini")
     port = int(os.environ.get("PORT", 5000))
     app = loadapp('config:development.ini', relative_to='.')
 
